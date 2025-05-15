3272dcf30080b23344da753c7307b18f59516441
Babasaner <37521122+Babasaner@users.noreply.github.com>
2025-05-15 19:42:50 +0200

    update

--- app/layout.tsx
+++ app/layout.tsx
@@ -20,10 +20,9 @@ export const metadata: Metadata = {
   icons: {
     icon: [
       { url: "/favicon.ico", sizes: "any" },
-      { url: "/favicon-16x16.png", type: "image/png", sizes: "16x16" },
-      { url: "/favicon-32x32.png", type: "image/png", sizes: "32x32" },
+      
     ],
-    apple: { url: "/apple-touch-icon.png" },
+   
   },
   manifest: "/site.webmanifest",
     generator: 'v0.dev'
